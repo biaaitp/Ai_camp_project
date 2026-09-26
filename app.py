df2ffb86d163bca53cd43cc83d393626d0a8e9a7
biaaitp <biaa.itp@gmail.com>
2026-09-26 19:06:24 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+model = joblib.load('tree_model.pkl')
+
+st.title("🍔 What Category Is It?")
+
+calories = st.slider("Calories", 0, 1000, 300)
+
+if st.button("Predict"):
+    result = model.predict([[calories]])
+    st.write("The AI thinks this is:", result[0])
+from google.colab import files
+files.download('app.py')
